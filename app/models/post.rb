145c5b238eb4edf29cd59071a806592c92a9a16e
Qibin <qbye0405@gmail.com>
2022-07-22 14:48:22 -0400

    commit tree 09a51976142444cf022b28707e7fc492760ebbc2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-fiction'] }
+
+    validate :clickbait
+
+    title_allow = title.include?("Won\'t Believe","Secret", "Top [number]", "Guess" )
+        def clickbait
+           if title_allow
+                render Post
+            else
+                render Post.errors.full_messages
+            end
+        end
 end
